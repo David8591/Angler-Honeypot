a9435c89fd474e01331322331f55749c5d3f41b9
David8591 <39322631+David8591@users.noreply.github.com>
2018-05-17 06:36:20 -0400

    Update toms_honeypot.py

--- toms_honeypot.py
+++ toms_honeypot.py
@@ -12,7 +12,7 @@
 #
 #     You should have received a copy of the GNU General Public License
 #     along with this program.  If not, see <http://www.gnu.org/licenses/>
-
+# var1 = sys.arg[0]
 import binascii
 import sys
 import time
